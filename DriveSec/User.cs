c12f77543c7d40d1c9002d36a3cfbb7f8b5d0058
Rubelewaskes <131644269+Rubelewaskes@users.noreply.github.com>
2024-04-12 17:31:36 +0300

    Шаблон

---
+++ DriveSec/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DriveSec;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public virtual ICollection<ChangeHistory> ChangeHistories { get; set; } = new List<ChangeHistory>();
+
+    public virtual ICollection<File> Files { get; set; } = new List<File>();
+
+    public virtual ICollection<UsersFolder> UsersFolders { get; set; } = new List<UsersFolder>();
+
+    public virtual ICollection<UsersMac> UsersMacs { get; set; } = new List<UsersMac>();
+}
